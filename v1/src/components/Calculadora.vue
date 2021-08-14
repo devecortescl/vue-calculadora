e0fdae44a35deaf78bc72ea592b043b414fd45b4
Esteban Cortes <esteban@quiebre.cl>
2021-08-14 00:48:06 -0400

    Actualizacion Calculadora Simple

--- v1/src/components/Calculadora.vue
+++ v1/src/components/Calculadora.vue
@@ -1,7 +1,7 @@
 <template>
   <div >
   <b-card
-    title="Sumas"
+    title="CALCULADORA SIMPLE V1"
     tag="article"
     style="max-width: 30rem;"
     class="mb-4"
@@ -11,12 +11,13 @@
       <div class="border">
       </div>
       <hr>
-      <input type="number" v-model="valueA"  class="form-control">
-
-      <input type="number" v-model="valueB"  class="form-control">
-
-      <hr>
-      {{(valueA)+(valueB)}}
+<input type="number" :value="value0" @input="numberone = $event.target.value" class="form-control" >
+<input type="number" :value="value1" @input="numbertwo = $event.target.value" class="form-control">
+<button class="btn btn-danger w-100 mt-2" v-on:click="operacion()">Resolver</button>
+<hr>
+<p class="alert alert-danger">Suma: {{mensaje}}</p>
+<p  class="alert alert-warning">Resta: {{mensaje2}}</p>
+<p  class="alert alert-primary">Multiplicación: {{mensaje3}}</p>
 
     </b-card-text>
 
@@ -28,18 +29,26 @@
 <script>
 export default {
   name: 'Calculadora',
-  props: {
-    msg: String
-  },
-  data(){
-    return{
-      valueA: 0,
-      valueB: 0,
-    }
-  },
-  computed:{
-
-  }
+  props: ['value0', 'value1'],
+     data: function () {
+        return {
+            numberone: null,
+            numbertwo: null,
+            mensaje: null,
+            mensaje2: null,
+            mensaje3: null
+        }
+    },
+    methods: {
+        operacion() {
+            this.mensaje = parseInt(this.numberone) +
+                parseInt(this.numbertwo);
+            this.mensaje2 = parseInt(this.numberone) -
+                parseInt(this.numbertwo);
+            this.mensaje3 = parseInt(this.numberone) *
+                parseInt(this.numbertwo);
+        }
+    },
 }
 </script>
 
