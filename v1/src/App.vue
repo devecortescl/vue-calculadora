54ed7fa42938b2787100253206ee2ad9e74c8d6a
Esteban Cortes <esteban@quiebre.cl>
2021-08-14 00:33:52 -0400

    Config Inicial
    Configuracion inicial del entorno vue.js

---
+++ v1/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app"> 
+    <div>
+      <h1> Calculadora</h1> <hr>
+    </div>
+    <div class="d-flex justify-content-center">
+   
+    <Calculadora />
+    </div>
+  </div>
+</template>
+
+<script>
+import Calculadora from './components/Calculadora.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Calculadora
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ v1/src/components/Calculadora.vue
@@ -0,0 +1,45 @@
+<template>
+  <div >
+  <b-card
+    title="Sumas"
+    tag="article"
+    style="max-width: 30rem;"
+    class="mb-4"
+  >
+    <b-card-text>
+    
+      <div class="border">
+      </div>
+      <hr>
+      <input type="number" v-model="valueA"  class="form-control">
+
+      <input type="number" v-model="valueB"  class="form-control">
+
+      <hr>
+      {{(valueA)+(valueB)}}
+
+    </b-card-text>
+
+  
+  </b-card>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Calculadora',
+  props: {
+    msg: String
+  },
+  data(){
+    return{
+      valueA: 0,
+      valueB: 0,
+    }
+  },
+  computed:{
+
+  }
+}
+</script>
+
